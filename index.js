7ea01d6c79d2512c04595855791b99623d7f35f5
Cody Brewer <brewer.cody.m@gmail.com>
2020-03-10 23:19:07 -0600

    set up express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const server = express();
+const PORT = 4000;
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.listen(PORT, () => {
+  console.log(`...server listening on port: ${PORT}`);
+});
